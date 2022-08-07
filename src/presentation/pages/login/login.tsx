3adc9a31da8805a6cb19fce2c6512aa2c13af78f
Fabrício Xavier Cerci <fabriciocerci@gmail.com>
2022-08-07 04:28:47 -0400

    feat: ensure Login will not load spinner and error on start

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Styles from './login-styles.scss'
 import { Footer, Input, FormStatus, LoginHeader as Header } from '@/presentation/components'
+import Context from '@/presentation/contexts/form/form-context'
+
+type StateProps = {
+  isLoading: boolean
+  errorMessage: string
+}
 
 const Login: React.FC = () => {
+  const [state] = useState<StateProps>({
+    isLoading: false,
+    errorMessage: ''
+  })
+
   return (
     <div className={Styles.login}>
       <Header />
+      <Context.Provider value={state}>
       <form className={Styles.form}>
         <h2>Login</h2>
         <Input type="email" name="email" placeholder="Digite o seu email" />
@@ -14,6 +26,7 @@ const Login: React.FC = () => {
         <span className={Styles.link}>Criar conta</span>
         <FormStatus />
       </form>
+      </Context.Provider>
       <Footer />
     </div>
   )
